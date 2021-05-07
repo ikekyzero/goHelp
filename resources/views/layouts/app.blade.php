e5460e0752434466432d839f9a77368163fcb681
Andrew Torgovkin <laere.cool@mail.ru>
2021-05-07 15:35:26 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,10 +66,10 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Теперь вы можете общаться с ним</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Ваша заявка отправлена!</h5>
       </div>
       <div class="modal-body">
-        Бурнашёва А.В. открыла вам чат, зайдите во вкладку чата.
+        Бурнашёва А.В. рассматривает вашу заявку помощи.
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
